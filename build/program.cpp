707f55cc256b4ff8f9a426b5cc948adcace6986c
宇星海 <1985375083@qq.com>
2026-01-03 20:43:21 +0800

    fix: 支持if和while语句(暂时不支持for语句)

--- build/program.cpp
+++ build/program.cpp
@@ -2,7 +2,15 @@ int main() {
     int name1 = 10;
     int name3 = 100;
     char name4 = 'a';
-    char  = 'b';
-
+    // 分支语句
+    if (name1 < name3) {
+        name3 = 200;
+    } else {
+        name3 = 300;
+    }
+    // 循环语句
+    while (name1 < 20) {
+        name1 = name1 + 1;
+    }
     return 0;
 }
